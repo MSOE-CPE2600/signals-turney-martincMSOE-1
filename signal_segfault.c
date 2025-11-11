846cac4c93946f811d4ad941fefe4bc6c6654b12
martinc <martinc@msoe.edu>
2025-11-11 16:24:11 -0600

    SIGSEGV Complete

--- signal_segfault.c
+++ signal_segfault.c
@@ -4,15 +4,29 @@
  */
 
 /**
- * Modified by:
+ * Modified by: Christopher Martin
  * 
- * Brief summary of modifications:
+ * Brief summary of modifications: created handle_segv() function to print out segmentation fault recieved
+ * WILL continuously print out that segfault was recieved and will NOT stop program
  */
 
 
 #include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void handle_segv(){
+    printf("Recieved segfault\n");
+    //exit(1);
+    return;
+}
 
 int main (int argc, char* argv[]) {
+
+    // Register signal
+    signal(SIGSEGV, handle_segv);
+
     // Declare a null pointer
     int* i = NULL;
 
