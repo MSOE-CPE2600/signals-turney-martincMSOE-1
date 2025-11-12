bbaad46b7f871974782bfad125cb5a784ab76a7a
martinc <martinc@msoe.edu>
2025-11-12 15:33:32 -0600

    SIGUSR1 Complete

--- signal_sigaction.c
+++ signal_sigaction.c
@@ -1,8 +1,39 @@
 /**
- * File: 
- * Modified by:
+ * File: signal_sigaction.c
+ * Modified by: Christopher Martin
  * 
- * Brief summary of program:
+ * Brief summary of program: the command "kill -SIGUSR1" can be called form a different terminal,
+ * this should call the handle_sigaction() method, print out the current process ID, and exit the program
  */
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void handle_sigaction() {
+    printf("Process ID: %d\n", getpid());
+    exit(1);
+}
+int main (){
+
+    // Register Signal and Sig Handler
+    struct sigaction sa;
+    sa.sa_handler = handle_sigaction; // declare which method is called when signal is thrown
+    sa.sa_flags = 0; // no special flags
+    sigemptyset(&sa.sa_mask); // no signal blocked during handler
+
+    // declare signal
+    sigaction(SIGUSR1, &sa, NULL);
+
+    // Wait in infinite loop
+    printf("Process ID: %d", getpid());
+    while(1){
+        printf("Waiting for SIGUSR1...\n");
+        sleep(1);
+    }
+    printf("Exiting Program\n");
+
+    return 0;
+}
 
 
